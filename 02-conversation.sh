e4d6bcd9b420ae9d165fb4cd0b7db6f35d6c8343
AdithyaAce <adithyagorapalli2005@gmail.com>
2026-05-28 21:51:20 +0530

    updated

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "musk:: hi modi sir, how are you?"
+echo "modi:: hi musk ! i am fine, what about you?"
+echo "musk:: i am good sir, i came here to talk to you"
+echo "modi:: tell me what you want from me?"
+echo "musk:: i want to built a tesla manfacturing unit in india?"
+echo "modi:: good to here it , i will send the required doc "
+echo "musk:: thank you modi ji!!"
